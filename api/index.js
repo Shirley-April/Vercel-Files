0bb5061c0090a966c3d0a1028a4514ab013f7409
Shirley-April <shirleyapril23@gmail.com>
2023-10-31 09:30:05 +0300

    store file in /tmp folder

--- api/index.js
+++ api/index.js
@@ -1,18 +1,40 @@
-const express = require('express')
+const express = require("express");
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
 
-const app = express()
-
+const app = express();
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => {
-    res.send("This is the /api route")
-})
+  res.send("This is the /api route");
+});
 
 app.get("/", (req, res) => {
-    res.send("This is the / route")
-})
+  res.send("This is the / route");
+});
 
-const PORT = 3000
 
-app.listen(PORT, () => console.log("App is running on port " + PORT))
+app.post("/file", async (req, res) => {
+  const { title, fileName } = req.body;
+  const doc = new PDFDocument();
 
-module.exports = app
+  let file = path.join(process.cwd(), "tmp", `${fileName}.pdf`);
+
+  let writeStream = fs.createWriteStream(file);
+
+  doc.pipe(writeStream);
+  doc.text(title);
+  doc.end();
+
+  writeStream.on("finish", function () {
+    res.sendFile(file)
+  });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log("App is running on port " + PORT));
+
+module.exports = app;
