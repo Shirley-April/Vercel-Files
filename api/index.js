5a29a39cd3a69c561fe71653b87f51c21086f37d
Shirley-April <shirleyapril23@gmail.com>
2023-10-31 09:47:16 +0300

    test again

--- api/index.js
+++ api/index.js
@@ -15,8 +15,12 @@ app.get("/", (req, res) => {
   res.send("This is the / route");
 });
 
+app.get("/api/post", (req, res) => {
+  res.send("This is the TEST ONE");
+});
+
 app.get("/post", (req, res) => {
-//   const body = req.body;
+  //   const body = req.body;
   res.send("hey POST GET REQuest");
 });
 
